c7c5ef7939c565160f08ca0e6fabd07ff925b381
None <None>
2023-12-14 23:02:00 -0800

    create arp and ethernet to send

---
+++ part4controller.py
@@ -0,0 +1,214 @@
+# Part 4 of UWCSE's Mininet-SDN project
+#
+# based on Lab Final from UCSC's Networking Class
+# which is based on of_tutorial by James McCauley
+
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.addresses import IPAddr, IPAddr6, EthAddr
+from pox.lib.packet.arp import arp
+from pox.lib.packet.ethernet import ethernet
+
+log = core.getLogger()
+
+# Convenience mappings of hostnames to ips
+IPS = {
+    "h10": "10.0.1.10",
+    "h20": "10.0.2.20",
+    "h30": "10.0.3.30",
+    "serv1": "10.0.4.10",
+    "hnotrust": "172.16.10.100",
+}
+
+MACS = {
+    "h10": "00:00:00:00:00:01",
+    "h20": "00:00:00:00:00:02",
+    "h30": "00:00:00:00:00:03",
+    "serv1": "00:00:00:00:00:04",
+    "hnotrust1": "00:00:00:00:00:05"
+}
+
+# Convenience mappings of hostnames to subnets
+SUBNETS = {
+    "h10": "10.0.1.0/24",
+    "h20": "10.0.2.0/24",
+    "h30": "10.0.3.0/24",
+    "serv1": "10.0.4.0/24",
+    "hnotrust": "172.16.10.0/24",
+}
+
+
+class Part4Controller(object):
+    """
+    A Connection object for that switch is passed to the __init__ function.
+    """
+
+    def __init__(self, connection):
+        print(connection.dpid)
+        # Keep track of the connection to the switch so that we can
+        # send it messages!
+        self.connection = connection
+
+        # This binds our PacketIn event listener
+        connection.addListeners(self)
+        # use the dpid to figure out what switch is being created
+        if connection.dpid == 1:
+            self.s1_setup()
+        elif connection.dpid == 2:
+            self.s2_setup()
+        elif connection.dpid == 3:
+            self.s3_setup()
+        elif connection.dpid == 21:
+            self.cores21_setup()
+        elif connection.dpid == 31:
+            self.dcs31_setup()
+        else:
+            print("UNKNOWN SWITCH")
+            exit(1)
+
+    def s1_setup(self):
+        # put switch 1 rules here
+
+        
+        #ARP
+        msg_arp = of.ofp_flow_mod()
+        msg_arp.match = of.ofp_match(dl_type=plib.ethernet.ARP_TYPE)
+        msg_arp.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        self.connection.send(msg_arp)
+
+        #Default
+        msg_default = of.ofp_flow_mod()
+        msg_default.match = of.ofp_match()
+        msg_default.actions.append(of.ofp_action_output(port=of.OFPP_NORMAL))
+        self.connection.send(msg_default)
+        pass
+
+    def s2_setup(self):
+        # put switch 2 rules here
+
+
+        #ARP
+        msg_arp = of.ofp_flow_mod()
+        msg_arp.match = of.ofp_match(dl_type=plib.ethernet.ARP_TYPE)
+        msg_arp.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        self.connection.send(msg_arp)
+
+        #Default
+        msg_default = of.ofp_flow_mod()
+        msg_default.match = of.ofp_match()
+        msg_default.actions.append(of.ofp_action_output(port=of.OFPP_NORMAL))
+        self.connection.send(msg_default)
+        pass
+
+    def s3_setup(self):
+        # put switch 3 rules here
+
+
+        #ARP
+        msg_arp = of.ofp_flow_mod()
+        msg_arp.match = of.ofp_match(dl_type=plib.ethernet.ARP_TYPE)
+        msg_arp.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
+        self.connection.send(msg_arp)
+
+        #Default
+        msg_default = of.ofp_flow_mod()
+        msg_default.match = of.ofp_match()
+        msg_default.actions.append(of.ofp_action_output(port=of.OFPP_NORMAL))
+        self.connection.send(msg_default)
+        pass
+
+    def cores21_setup(self):
+        # put core switch rules here
+
+        # Block no trust
+        msg_notrust = of.ofp_flow_mod()
+        msg_notrust.match = of.ofp_match(
+            dl_type=plib.ethernet.IP_TYPE,
+            nw_proto=plib.ipv4.ICMP_PROTOCOL,
+            nw_src="172.16.10.100"
+        )  
+        self.connection.send(msg_notrust)
+        
+
+    def dcs31_setup(self):
+        # put datacenter switch rules here
+
+        # Block no trust
+        msg_notrust = of.ofp_flow_mod()
+        msg_notrust.match = of.ofp_match(
+            dl_type=plib.ethernet.IP_TYPE,
+            nw_src="172.16.10.100"
+        )  
+        self.connection.send(msg_notrust)
+
+        #Default
+        msg_default = of.ofp_flow_mod()
+        msg_default.match = of.ofp_match()
+        msg_default.actions.append(of.ofp_action_output(port=of.OFPP_NORMAL))
+        self.connection.send(msg_default)
+        pass
+# ---------------------------------------------- #
+    def create_ethernet(self, packet):
+        new_network = ethernet()
+        new_network.type = ethernet.ARP_TYPE
+        new_network.src = "00:00:00:00:00:06"
+        new_network.dst = packet.src
+        new_network.payload = reply
+        return new_network
+
+    def handle_arp(self):
+        packet = event.parsed
+
+
+        # Cài các parameter cho gói arp
+        msg_arp = arp()
+        msg_arp.hwtype = 1
+        msg_arp.opcode = arp.REPLY
+        msg_arp.hwsrc = "00:00:00:00:00:06"
+        msg_arp.hwdst = packet.src
+        msg_arp.protosrc = packet.payload.protodst
+        msg_arp.protodst = packet.payload.protosrc
+
+        # Tạo ethernet để gửi đi
+        new_network = create_ethernet(packet)
+
+# ---------------------------------------------- #
+
+
+    # used in part 4 to handle individual ARP packets
+    # not needed for part 3 (USE RULES!)
+    # causes the switch to output packet_in on out_port
+    def resend_packet(self, packet_in, out_port):
+        msg = of.ofp_packet_out()
+        msg.data = packet_in
+        action = of.ofp_action_output(port=out_port)
+        msg.actions.append(action)
+        self.connection.send(msg)
+
+    def _handle_PacketIn(self, event):
+        """
+        Packets not handled by the router rules will be
+        forwarded to this method to be handled by the controller
+        """
+
+        packet = event.parsed  # This is the parsed packet data.
+        if not packet.parsed:
+            log.warning("Ignoring incomplete packet")
+            return
+
+        packet_in = event.ofp  # The actual ofp_packet_in message.
+        print(
+            "Unhandled packet from " + str(self.connection.dpid) + ":" + packet.dump()
+        )
+
+
+def launch():
+    """
+    Starts the component
+    """
+
+    def start_switch(event):
+        log.debug("Controlling %s" % (event.connection,))
+        Part4Controller(event.connection)
+
+    core.openflow.addListenerByName("ConnectionUp", start_switch)
